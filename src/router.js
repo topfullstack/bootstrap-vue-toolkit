4c7876e5e4840d880928722e6bc7032d7f3d659f
wxs77577 <wu-xuesong@qq.com>
2018-09-20 23:11:37 +0800

    update docs

--- src/router.js
+++ src/router.js
@@ -11,7 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      redirect: 'FormBuilder',
+      redirect: '/page/FormBuilder',
     },
     {
       path: '/page/FormBuilder',
