a49027a53a7af7619e1139db2f433f42b16f9936
wxs77577 <wu-xuesong@qq.com>
2018-09-29 09:24:29 +0800

    update

--- src/components/FormHtml.vue
+++ src/components/FormHtml.vue
@@ -1,16 +1,20 @@
 <template>
-  <html-editor :value="value" @input="$emit('input', arguments[0])" :options="options" />
+  <!-- <quill-editor :value="value" @input="$emit('input', arguments[0])" :options="options" /> -->
+  <vue-html5-editor :content="value" @change="$emit('input', arguments[0])" :options="options" />
 </template>
 
 <script>
-import "quill/dist/quill.core.css";
-import "quill/dist/quill.snow.css";
+// import "quill/dist/quill.core.css";
+// import "quill/dist/quill.snow.css";
 // import "quill/dist/quill.bubble.css";
-import { quillEditor } from "vue-quill-editor";
+// import { quillEditor } from "vue-quill-editor";
+import 'font-awesome/css/font-awesome.css'
+import VueHtml5Editor from 'vue-html5-editor'
 
 export default {
   components: {
-    HtmlEditor: quillEditor
+    // HtmlEditor: quillEditor
+    VueHtml5Editor: new VueHtml5Editor({})
   },
   props: {
     value: String,
@@ -25,3 +29,13 @@ export default {
   }
 };
 </script>
+
+<style>
+.vue-html5-editor>.toolbar>.dashboard button{
+  line-height: 1em;
+
+}
+.vue-html5-editor>.toolbar>.dashboard input[type=text], .vue-html5-editor>.toolbar>.dashboard input[type=number], .vue-html5-editor>.toolbar>.dashboard select {
+  line-height: 1em;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const isDocEnv = process.env.NODE_ENV === 'docs'
 module.exports = {
   baseUrl: isDocEnv ? '/bootstrap-vue-toolkit/' : '/',
   outputDir: isDocEnv ? 'docs' : 'dist',
+  productionSourceMap: false,
   runtimeCompiler: true,
 }
